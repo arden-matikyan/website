66d3499ecea9c802bac63e7a31b8e33b1ae80155
Arden <ardenmatikyan@gmail.com>
2025-09-02 14:40:53 -0400

    First push

---
+++ script.js
@@ -0,0 +1,10 @@
+function toggleMenu() {
+    document.getElementById('menu').classList.toggle('active');
+ 
+    
+}
+function switchpage() {
+    document.getElementById('menu').classList.toggle('active');
+ 
+    
+}
